endlessm/eos-knowledge-lib

eknWebview: Add support for named blobs in records
aka "bundled resources"

This allows us to add CSS files, logos, etc. inside the shard instead of
in eos-knowledge-lib, allowing us to change them in future versions
without wrecking the content.

js/search/engine.js
@@ -78,6 +78,24 @@ const Engine = Lang.Class({
         this._runtime_objects = new Map();
     },
 
+    load_record_by_id: function (id, cancellable, callback) {
+        let task = new AsyncTask.AsyncTask(this, cancellable, callback);
+        task.catch_errors(() => {
+            let [domain, hash] = Utils.components_from_ekn_id(id);
+            let domain_obj = this._get_domain(domain);
+
+            domain_obj.load_record_from_hash(hash, cancellable, task.catch_callback_errors((domain_obj, domain_task) => {
+                let model = domain_obj.load_record_by_id_finish(domain_task);
+                task.return_value(model);
+            }));
+        });
+        return task;
+    },
+
+    load_record_by_id_finish: function (task) {
+        return task.finish();
+    },
+
     /**
      * Function: get_object_by_id
      *

ptomato/ekncontent

js/search/engine.js
@@ -76,6 +76,24 @@ const Engine = Lang.Class({
         this._domain_cache = {};
     },
 
+    load_record_by_id: function (id, cancellable, callback) {
+        let task = new AsyncTask.AsyncTask(this, cancellable, callback);
+        task.catch_errors(() => {
+            let [domain, hash] = Utils.components_from_ekn_id(id);
+            let domain_obj = this._get_domain(domain);
+
+            domain_obj.load_record_from_hash(hash, cancellable, task.catch_callback_errors((domain_obj, domain_task) => {
+                let model = domain_obj.load_record_from_hash_finish(domain_task);
+                task.return_value(model);
+            }));
+        });
+        return task;
+    },
+
+    load_record_by_id_finish: function (task) {
+        return task.finish();
+    },
+
     /**
      * Function: get_object_by_id
      *